fda03886b9755f41278acd4ccc3fe847a3257ada
templateshai <yuhairui@guoshujinfu.com>
2018-04-28 18:06:18 +0800

    add sushan project

---
+++ src/main/java/com/river/bend/App.java
@@ -0,0 +1,13 @@
+package com.river.bend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
